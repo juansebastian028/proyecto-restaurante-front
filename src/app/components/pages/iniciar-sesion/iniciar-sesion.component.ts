bc580e8bdfe50684253126921feb28ba887905d6
Juan Maya <carlosmaya326@gmail.com>
2021-04-10 17:37:44 -0500

    Diseño de login

--- src/app/components/pages/iniciar-sesion/iniciar-sesion.component.ts
+++ src/app/components/pages/iniciar-sesion/iniciar-sesion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-iniciar-sesion',
@@ -10,6 +11,10 @@ export class IniciarSesionComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(f: NgForm) {
+    console.log(f.value);
   }
 
 }
